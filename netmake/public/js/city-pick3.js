72e43e4f9d071377db62e00200658b303f0de3f9
babalaile <33535811+babalaile@users.noreply.github.com>
2018-06-20 17:04:12 +0800

    Add files via upload

---
+++ netmake/public/js/city-pick3.js
@@ -0,0 +1,686 @@
+// jshint ignore: start
++
+function(e) {
+    e.rawCitiesData = [{
+        name: "湖北省",
+        code: "420000",
+        sub: [{
+            name: "武汉市",
+            code: "420100",
+            sub: [{
+                name: "市辖区",
+                code: "420101"
+            },
+            {
+                name: "江岸区",
+                code: "420102"
+            },
+            {
+                name: "江汉区",
+                code: "420103"
+            },
+            {
+                name: "硚口区",
+                code: "420104"
+            },
+            {
+                name: "汉阳区",
+                code: "420105"
+            },
+            {
+                name: "武昌区",
+                code: "420106"
+            },
+            {
+                name: "青山区",
+                code: "420107"
+            },
+            {
+                name: "洪山区",
+                code: "420111"
+            },
+            {
+                name: "东西湖区",
+                code: "420112"
+            },
+            {
+                name: "汉南区",
+                code: "420113"
+            },
+            {
+                name: "蔡甸区",
+                code: "420114"
+            },
+            {
+                name: "江夏区",
+                code: "420115"
+            },
+            {
+                name: "黄陂区",
+                code: "420116"
+            },
+            {
+                name: "新洲区",
+                code: "420117"
+            }]
+        },
+        {
+            name: "黄石市",
+            code: "420200",
+            sub: [{
+                name: "市辖区",
+                code: "420201"
+            },
+            {
+                name: "黄石港区",
+                code: "420202"
+            },
+            {
+                name: "西塞山区",
+                code: "420203"
+            },
+            {
+                name: "下陆区",
+                code: "420204"
+            },
+            {
+                name: "铁山区",
+                code: "420205"
+            },
+            {
+                name: "阳新县",
+                code: "420222"
+            },
+            {
+                name: "大冶市",
+                code: "420281"
+            }]
+        },
+        {
+            name: "十堰市",
+            code: "420300",
+            sub: [{
+                name: "市辖区",
+                code: "420301"
+            },
+            {
+                name: "茅箭区",
+                code: "420302"
+            },
+            {
+                name: "张湾区",
+                code: "420303"
+            },
+            {
+                name: "郧阳区",
+                code: "420304"
+            },
+            {
+                name: "郧西县",
+                code: "420322"
+            },
+            {
+                name: "竹山县",
+                code: "420323"
+            },
+            {
+                name: "竹溪县",
+                code: "420324"
+            },
+            {
+                name: "房县",
+                code: "420325"
+            },
+            {
+                name: "丹江口市",
+                code: "420381"
+            }]
+        },
+        {
+            name: "宜昌市",
+            code: "420500",
+            sub: [{
+                name: "市辖区",
+                code: "420501"
+            },
+            {
+                name: "西陵区",
+                code: "420502"
+            },
+            {
+                name: "伍家岗区",
+                code: "420503"
+            },
+            {
+                name: "点军区",
+                code: "420504"
+            },
+            {
+                name: "猇亭区",
+                code: "420505"
+            },
+            {
+                name: "夷陵区",
+                code: "420506"
+            },
+            {
+                name: "远安县",
+                code: "420525"
+            },
+            {
+                name: "兴山县",
+                code: "420526"
+            },
+            {
+                name: "秭归县",
+                code: "420527"
+            },
+            {
+                name: "长阳土家族自治县",
+                code: "420528"
+            },
+            {
+                name: "五峰土家族自治县",
+                code: "420529"
+            },
+            {
+                name: "宜都市",
+                code: "420581"
+            },
+            {
+                name: "当阳市",
+                code: "420582"
+            },
+            {
+                name: "枝江市",
+                code: "420583"
+            }]
+        },
+        {
+            name: "襄阳市",
+            code: "420600",
+            sub: [{
+                name: "市辖区",
+                code: "420601"
+            },
+            {
+                name: "襄城区",
+                code: "420602"
+            },
+            {
+                name: "樊城区",
+                code: "420606"
+            },
+            {
+                name: "襄州区",
+                code: "420607"
+            },
+            {
+                name: "南漳县",
+                code: "420624"
+            },
+            {
+                name: "谷城县",
+                code: "420625"
+            },
+            {
+                name: "保康县",
+                code: "420626"
+            },
+            {
+                name: "老河口市",
+                code: "420682"
+            },
+            {
+                name: "枣阳市",
+                code: "420683"
+            },
+            {
+                name: "宜城市",
+                code: "420684"
+            }]
+        },
+        {
+            name: "鄂州市",
+            code: "420700",
+            sub: [{
+                name: "市辖区",
+                code: "420701"
+            },
+            {
+                name: "梁子湖区",
+                code: "420702"
+            },
+            {
+                name: "华容区",
+                code: "420703"
+            },
+            {
+                name: "鄂城区",
+                code: "420704"
+            }]
+        },
+        {
+            name: "荆门市",
+            code: "420800",
+            sub: [{
+                name: "市辖区",
+                code: "420801"
+            },
+            {
+                name: "东宝区",
+                code: "420802"
+            },
+            {
+                name: "掇刀区",
+                code: "420804"
+            },
+            {
+                name: "京山县",
+                code: "420821"
+            },
+            {
+                name: "沙洋县",
+                code: "420822"
+            },
+            {
+                name: "钟祥市",
+                code: "420881"
+            }]
+        },
+        {
+            name: "孝感市",
+            code: "420900",
+            sub: [{
+                name: "市辖区",
+                code: "420901"
+            },
+            {
+                name: "孝南区",
+                code: "420902"
+            },
+            {
+                name: "孝昌县",
+                code: "420921"
+            },
+            {
+                name: "大悟县",
+                code: "420922"
+            },
+            {
+                name: "云梦县",
+                code: "420923"
+            },
+            {
+                name: "应城市",
+                code: "420981"
+            },
+            {
+                name: "安陆市",
+                code: "420982"
+            },
+            {
+                name: "汉川市",
+                code: "420984"
+            }]
+        },
+        {
+            name: "荆州市",
+            code: "421000",
+            sub: [{
+                name: "市辖区",
+                code: "421001"
+            },
+            {
+                name: "沙市区",
+                code: "421002"
+            },
+            {
+                name: "荆州区",
+                code: "421003"
+            },
+            {
+                name: "公安县",
+                code: "421022"
+            },
+            {
+                name: "监利县",
+                code: "421023"
+            },
+            {
+                name: "江陵县",
+                code: "421024"
+            },
+            {
+                name: "石首市",
+                code: "421081"
+            },
+            {
+                name: "洪湖市",
+                code: "421083"
+            },
+            {
+                name: "松滋市",
+                code: "421087"
+            }]
+        },
+        {
+            name: "黄冈市",
+            code: "421100",
+            sub: [{
+                name: "市辖区",
+                code: "421101"
+            },
+            {
+                name: "黄州区",
+                code: "421102"
+            },
+            {
+                name: "团风县",
+                code: "421121"
+            },
+            {
+                name: "红安县",
+                code: "421122"
+            },
+            {
+                name: "罗田县",
+                code: "421123"
+            },
+            {
+                name: "英山县",
+                code: "421124"
+            },
+            {
+                name: "浠水县",
+                code: "421125"
+            },
+            {
+                name: "蕲春县",
+                code: "421126"
+            },
+            {
+                name: "黄梅县",
+                code: "421127"
+            },
+            {
+                name: "麻城市",
+                code: "421181"
+            },
+            {
+                name: "武穴市",
+                code: "421182"
+            }]
+        },
+        {
+            name: "咸宁市",
+            code: "421200",
+            sub: [{
+                name: "市辖区",
+                code: "421201"
+            },
+            {
+                name: "咸安区",
+                code: "421202"
+            },
+            {
+                name: "嘉鱼县",
+                code: "421221"
+            },
+            {
+                name: "通城县",
+                code: "421222"
+            },
+            {
+                name: "崇阳县",
+                code: "421223"
+            },
+            {
+                name: "通山县",
+                code: "421224"
+            },
+            {
+                name: "赤壁市",
+                code: "421281"
+            }]
+        },
+        {
+            name: "随州市",
+            code: "421300",
+            sub: [{
+                name: "市辖区",
+                code: "421301"
+            },
+            {
+                name: "曾都区",
+                code: "421303"
+            },
+            {
+                name: "随县",
+                code: "421321"
+            },
+            {
+                name: "广水市",
+                code: "421381"
+            }]
+        },
+        {
+            name: "恩施土家族苗族自治州",
+            code: "422800",
+            sub: [{
+                name: "恩施市",
+                code: "422801"
+            },
+            {
+                name: "利川市",
+                code: "422802"
+            },
+            {
+                name: "建始县",
+                code: "422822"
+            },
+            {
+                name: "巴东县",
+                code: "422823"
+            },
+            {
+                name: "宣恩县",
+                code: "422825"
+            },
+            {
+                name: "咸丰县",
+                code: "422826"
+            },
+            {
+                name: "来凤县",
+                code: "422827"
+            },
+            {
+                name: "鹤峰县",
+                code: "422828"
+            }]
+        },
+        {
+            name: "仙桃市",
+            code: "429004"
+        },
+        {
+            name: "潜江市",
+            code: "429005"
+        },
+        {
+            name: "天门市",
+            code: "429006"
+        },
+        {
+            name: "神农架林区",
+            code: "429021"
+        }]
+    }]
+} ($), +
+function(e) {
+    "use strict";
+    var n, a = e.rawCitiesData,
+    c = function(e) {
+        for (var n = [], a = 0; a < e.length; a++) {
+            var c = e[a];
+            /^请选择|市辖区/.test(c.name) || n.push(c)
+        }
+        return n.length ? n: []
+    },
+    o = function(e) {
+        return e.sub ? c(e.sub) : [{
+            name: "",
+            code: e.code
+        }]
+    },
+    m = function(e) {
+        for (var n = 0; n < a.length; n++) if (a[n].code === e || a[n].name === e) return o(a[n]);
+        return []
+    },
+    d = function(e, n) {
+        for (var c = 0; c < a.length; c++) if (a[c].code === e || a[c].name === e) for (var m = 0; m < a[c].sub.length; m++) if (a[c].sub[m].code === n || a[c].sub[m].name === n) return o(a[c].sub[m])
+    },
+    u = function(e) {
+        var n, c, o = a[0],
+        m = e.split(" ");
+        return a.map(function(e) {
+            e.name === m[0] && (o = e)
+        }),
+        o.sub.map(function(e) {
+            e.name === m[1] && (n = e)
+        }),
+        m[2] && n.sub.map(function(e) {
+            e.name === m[2] && (c = e)
+        }),
+        c ? [o.code, n.code, c.code] : [o.code, n.code]
+    };
+    e.fn.cityPicker = function(c) {
+        return c = e.extend({},
+        n, c),
+        this.each(function() {
+            var n = this,
+            s = a.map(function(e) {
+                return e.name
+            }),
+            b = a.map(function(e) {
+                return e.code
+            }),
+            t = o(a[0]),
+            r = t.map(function(e) {
+                return e.name
+            }),
+            i = t.map(function(e) {
+                return e.code
+            }),
+            l = o(a[0].sub[0]),
+            f = l.map(function(e) {
+                return e.name
+            }),
+            p = l.map(function(e) {
+                return e.code
+            }),
+            v = s[0],
+            h = r[0],
+            V = f[0],
+            y = [{
+                displayValues: s,
+                values: b,
+                cssClass: "col-province"
+            },
+            {
+                displayValues: r,
+                values: i,
+                cssClass: "col-city"
+            }];
+            c.showDistrict && y.push({
+                values: p,
+                displayValues: f,
+                cssClass: "col-district"
+            });
+            var g = {
+                cssClass: "city-picker",
+                rotateEffect: !1,
+                formatValue: function(e, n, a) {
+                    return a.join(" ")
+                },
+                onChange: function(a, o, u) {
+                    var s, b = a.cols[0].displayValue;
+                    if (b !== v) {
+                        var t = m(b);
+                        s = t[0].name;
+                        var r = d(b, s);
+                        return a.cols[1].replaceValues(t.map(function(e) {
+                            return e.code
+                        }), t.map(function(e) {
+                            return e.name
+                        })),
+                        c.showDistrict && a.cols[2].replaceValues(r.map(function(e) {
+                            return e.code
+                        }), r.map(function(e) {
+                            return e.name
+                        })),
+                        v = b,
+                        h = s,
+                        a.updateValue(),
+                        !1
+                    }
+                    if (c.showDistrict && (s = a.cols[1].displayValue, s !== h)) {
+                        var i = d(b, s);
+                        return a.cols[2].replaceValues(i.map(function(e) {
+                            return e.code
+                        }), i.map(function(e) {
+                            return e.name
+                        })),
+                        h = s,
+                        a.updateValue(),
+                        !1
+                    }
+                    e(n).attr("data-code", o[o.length - 1]),
+                    e(n).attr("data-codes", o.join(",")),
+                    c.onChange && c.onChange.call(n, a, o, u)
+                },
+                cols: y
+            };
+            if (this) {
+                var C = e.extend({},
+                c, g),
+                w = e(this).val();
+                if (w || (w = "湖北省 武汉市 江岸区"), v = w.split(" ")[0], h = w.split(" ")[1], V = w.split(" ")[2], w) {
+                    if (C.value = u(w), C.value[0]) {
+                        var D = m(C.value[0]);
+                        C.cols[1].values = D.map(function(e) {
+                            return e.code
+                        }),
+                        C.cols[1].displayValues = D.map(function(e) {
+                            return e.name
+                        })
+                    }
+                    if (C.value[1]) {
+                        if (c.showDistrict) {
+                            var k = d(C.value[0], C.value[1]);
+                            C.cols[2].values = k.map(function(e) {
+                                return e.code
+                            }),
+                            C.cols[2].displayValues = k.map(function(e) {
+                                return e.name
+                            })
+                        }
+                    } else if (c.showDistrict) {
+                        var k = d(C.value[0], C.cols[1].values[0]);
+                        C.cols[2].values = k.map(function(e) {
+                            return e.code
+                        }),
+                        C.cols[2].displayValues = k.map(function(e) {
+                            return e.name
+                        })
+                    }
+                }
+                e(this).picker(C)
+            }
+        })
+    },
+    n = e.fn.cityPicker.prototype.defaults = {
+        showDistrict: !0
+    }
+} ($);
